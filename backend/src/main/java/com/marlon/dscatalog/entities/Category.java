b8bafc30cad8dc5aa9b91d88170b5a4e4258d9ad
Marlon <frazao.marlon@gmail.com>
2022-06-18 14:05:38 -0300

    JPA Mapping

--- backend/src/main/java/com/marlon/dscatalog/entities/Category.java
+++ backend/src/main/java/com/marlon/dscatalog/entities/Category.java
@@ -2,12 +2,22 @@ package com.marlon.dscatalog.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import com.marlon.dscatalog.dto.CategoryDTO;
 import com.marlon.dscatalog.utils.Convertible;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable, Convertible<CategoryDTO, Long> {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
