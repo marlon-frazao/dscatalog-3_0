fafc9bd32d3eb15be58e8333518a493b38791c74
Marlon <frazao.marlon@gmail.com>
2022-06-18 13:26:45 -0300

    IService interface

---
+++ backend/src/main/java/com/marlon/dscatalog/services/IService.java
@@ -0,0 +1,15 @@
+package com.marlon.dscatalog.services;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import com.marlon.dscatalog.utils.Convertible;
+
+@Service
+public interface IService<T extends Convertible<DTO, ID>, DTO extends Convertible<T, ID>, ID> {
+
+	JpaRepository<T, ID> getRepository();
+	Page<DTO> findAllPaged(Object filter, Pageable pageable);
+}
